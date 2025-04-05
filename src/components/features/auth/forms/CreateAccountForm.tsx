6e4eab8041b45b914ac5a0923533a90a3721a61e
key-forr <denyskifor@gmail.com>
2025-04-05 18:10:19 +0300

    Added alert component to display a confirmation message that arrives in the mail

--- src/components/features/auth/forms/CreateAccountForm.tsx
+++ src/components/features/auth/forms/CreateAccountForm.tsx
@@ -1,9 +1,16 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { CircleCheck } from 'lucide-react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 
+import {
+	Alert,
+	AlertDescription,
+	AlertTitle
+} from '@/components/ui/common/Alert'
 import { Button } from '@/components/ui/common/Button'
 import {
 	Form,
@@ -25,6 +32,8 @@ import {
 import { AuthWrapper } from '../AuthWrapper'
 
 export function CreateAccountForm() {
+	const [isSuccess, setIsSuccess] = useState(false)
+
 	const form = useForm<TypeCreateAccountSchema>({
 		resolver: zodResolver(createAccountSchema),
 		defaultValues: {
@@ -36,7 +45,7 @@ export function CreateAccountForm() {
 
 	const [create, { loading: isLoadingCreate }] = useCreateUserMutation({
 		onCompleted() {
-			toast.success('Успішна реєстрація')
+			setIsSuccess(true)
 		},
 		onError() {
 			toast.error('Помилка при реєстрації')
@@ -55,77 +64,85 @@ export function CreateAccountForm() {
 			backButtonLabel='Уже зареєстровані? Увійти'
 			backButtonHref='/account/login'
 		>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className='grid gap-y-3'
-				>
-					<FormField
-						control={form.control}
-						name='username'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Ім'я користувача</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='johndoe'
-										disabled={isLoadingCreate}
-										{...field}
-									/>
-								</FormControl>
-								<FormDescription>
-									Поля для вводу імені
-								</FormDescription>
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='email'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Пошта</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='john.doe@example.com'
-										disabled={isLoadingCreate}
-										{...field}
-									/>
-								</FormControl>
-								<FormDescription>
-									Поля для вводу пошти
-								</FormDescription>
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='password'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Пароль</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='********'
-										type='password'
-										disabled={isLoadingCreate}
-										{...field}
-									/>
-								</FormControl>
-								<FormDescription>
-									Поля для вводу пароля
-								</FormDescription>
-							</FormItem>
-						)}
-					/>
-					<Button
-						className='mt-2 w-full'
-						disabled={!isValid || isLoadingCreate}
+			{isSuccess ? (
+				<Alert>
+					<CircleCheck className='size-4' />
+					<AlertTitle>Success</AlertTitle>
+					<AlertDescription>Confirm Email</AlertDescription>
+				</Alert>
+			) : (
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className='grid gap-y-3'
 					>
-						Продовжити
-					</Button>
-				</form>
-			</Form>
+						<FormField
+							control={form.control}
+							name='username'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Ім'я користувача</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='johndoe'
+											disabled={isLoadingCreate}
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription>
+										Поля для вводу імені
+									</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='email'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Пошта</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='john.doe@example.com'
+											disabled={isLoadingCreate}
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription>
+										Поля для вводу пошти
+									</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='password'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Пароль</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='********'
+											type='password'
+											disabled={isLoadingCreate}
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription>
+										Поля для вводу пароля
+									</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<Button
+							className='mt-2 w-full'
+							disabled={!isValid || isLoadingCreate}
+						>
+							Продовжити
+						</Button>
+					</form>
+				</Form>
+			)}
 		</AuthWrapper>
 	)
 }
