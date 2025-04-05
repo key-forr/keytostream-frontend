ad144461f829734daa6dc3a11d44d69fc8ff7a39
key-forr <denyskifor@gmail.com>
2025-04-05 19:16:21 +0300

    page for verification account with token

--- src/components/features/auth/forms/VerifyAccountForm.tsx
+++ src/components/features/auth/forms/VerifyAccountForm.tsx
@@ -1,3 +1,46 @@
+'use client'
+
+import { Loader } from 'lucide-react'
+import { useTranslations } from 'next-intl'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { toast } from 'sonner'
+
+import { useVerifyAccountMutation } from '@/graphql/generated/output'
+
+import { AuthWrapper } from '../AuthWrapper'
+
 export function VerifyAccountForm() {
-	return <div>VerifyAccountForm</div>
+	const t = useTranslations('auth.verify')
+
+	const router = useRouter()
+	const searchParams = useSearchParams()
+
+	const token = searchParams.get('token') ?? ''
+
+	const [verify] = useVerifyAccountMutation({
+		onCompleted() {
+			toast.success(t('successMessage'))
+			router.push('/dashboard/settings')
+		},
+		onError() {
+			toast.error(t('errorMessage'))
+		}
+	})
+
+	useEffect(() => {
+		verify({
+			variables: {
+				data: { token }
+			}
+		})
+	}, [token])
+
+	return (
+		<AuthWrapper heading={t('heading')}>
+			<div className='flex justify-center'>
+				<Loader className='size-8 animate-spin' />
+			</div>
+		</AuthWrapper>
+	)
 }
