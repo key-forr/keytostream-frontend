76f700264c701af5f6815781f216abe00ebedfc0
key-forr <denyskifor@gmail.com>
2025-04-05 14:53:08 +0300

    install package, added theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@
 
 import { useTranslations } from 'next-intl'
 
-import { Button } from '@/components/ui/common/Button'
-
 export default function Home() {
 	const t = useTranslations('home')
 
-	return <div></div>
+	return <div>Home</div>
 }
