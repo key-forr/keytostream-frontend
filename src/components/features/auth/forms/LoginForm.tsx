ee0e76cfa6b398d8695f1cadf35b80f47fa35d1f
key-forr <denyskifor@gmail.com>
2025-04-06 18:45:19 +0300

    create temp form

--- src/components/features/auth/forms/LoginForm.tsx
+++ src/components/features/auth/forms/LoginForm.tsx
@@ -1,3 +1,46 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useTranslations } from 'next-intl'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import { useLoginUserMutation } from '@/graphql/generated/output'
+
+import { TypeLoginSchema, loginSchema } from '@/schemas/auth/login.schema'
+
+import { AuthWrapper } from '../AuthWrapper'
+
 export function LoginForm() {
-	return <div>LoginForm</div>
+	const t = useTranslations('auth.login')
+
+	const form = useForm<TypeLoginSchema>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			login: '',
+			password: ''
+		}
+	})
+
+	const [login, { loading: isLoadingLogin }] = useLoginUserMutation({
+		onCompleted() {},
+		onError() {
+			toast.error(t('errorMessage'))
+		}
+	})
+
+	const { isValid } = form.formState
+
+	function onSubmit(data: TypeLoginSchema) {
+		login({ variables: { data } })
+	}
+	return (
+		<AuthWrapper
+			heading={t('heading')}
+			backButtonLabel={t('backButtonLabel')}
+			backButtonHref='/account/create'
+		>
+			LoginForm
+		</AuthWrapper>
+	)
 }
