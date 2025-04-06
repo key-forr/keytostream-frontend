3a38d3eb1a5326600f68468150c7bdaac6075e42
key-forr <denyskifor@gmail.com>
2025-04-06 19:32:42 +0300

    Added login via TOTP confirmation and created a beautiful form for it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,15 @@ const config: Config = {
 					'4': 'hsl(var(--chart-4))',
 					'5': 'hsl(var(--chart-5))'
 				}
+			},
+			keyframes: {
+				'caret-blink': {
+					'0%,70%,100%': { opacity: '1' },
+					'20%,50%': { opacity: '0' }
+				}
+			},
+			animation: {
+				'caret-blink': 'caret-blink 1.25s ease-out infinite'
 			}
 		}
 	},
