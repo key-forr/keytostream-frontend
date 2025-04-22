0cfcd8e8b449e1fc36a1d41eecdcf48bfcd3d0f3
key-forr <denyskifor@gmail.com>
2025-04-22 15:43:44 +0300

    added component heading for settings page

---
+++ src/components/ui/elements/Heading.tsx
@@ -0,0 +1,45 @@
+import { type VariantProps, cva } from 'class-variance-authority'
+
+import { FindProfileQuery } from '@/graphql/generated/output'
+
+import { getMediaSource } from '@/utils/get-media-source'
+import { cn } from '@/utils/tw-merge'
+
+import { Avatar, AvatarFallback, AvatarImage } from '../common/Avatar'
+
+const headingSizes = cva('', {
+	variants: {
+		size: {
+			sm: 'text-lg',
+			default: 'text-2xl',
+			lg: 'size-4xl',
+			xl: 'text-5xl'
+		}
+	},
+	defaultVariants: {
+		size: 'default'
+	}
+})
+
+interface HeadingProps extends VariantProps<typeof headingSizes> {
+	title: string
+	description?: string
+}
+
+export function Heading({ size, title, description }: HeadingProps) {
+	return (
+		<div className='space-y-2'>
+			<h1
+				className={cn(
+					'font-semibold text-foreground',
+					headingSizes({ size })
+				)}
+			>
+				{title}
+			</h1>
+			{description && (
+				<p className='text-muted-foreground'>{description}</p>
+			)}
+		</div>
+	)
+}
