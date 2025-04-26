9d0ceb25d094e124b7722f5d239fc1a33acc3172
key-forr <denyskifor@gmail.com>
2025-04-26 18:17:12 +0300

    deactivate page, zod schema, mutation

---
+++ src/libs/constants/seo.constants.ts
@@ -0,0 +1,16 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'keytostream'
+
+export const SITE_DESCRIPTION =
+	'keytostream — це платформа для прямих трансляцій, яка об’єднує стрімерів і глядачів. Діліться своїми захопленнями та приєднуйтеся до спільноти keytostream!'
+
+export const SITE_KEYWORDS = [
+	'keytostream',
+	'платформа для прямих трансляцій',
+	'стрімінг',
+	'прямі трансляції',
+	'взаємодія з глядачами',
+	'відеоконтент',
+	'онлайн-спільнота'
+]
